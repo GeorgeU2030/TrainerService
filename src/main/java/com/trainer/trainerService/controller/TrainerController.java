cbf334e773e82eec0ed42060e58778561183e0dc
CarlosJaBR <carjabo12@gmail.com>
2025-03-15 16:29:50 -0500

    add keycloak and jwt configuration

--- src/main/java/com/trainer/trainerService/controller/TrainerController.java
+++ src/main/java/com/trainer/trainerService/controller/TrainerController.java
@@ -3,6 +3,7 @@ package com.trainer.trainerService.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,16 +21,19 @@ public class TrainerController {
     private TrainerService trainerService;
 
     @PostMapping
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public Trainer addTrainer(@RequestBody Trainer trainer) {
         return trainerService.addTrainer(trainer);
     }
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_TRAINER','ROLE_MEMBER')")
     public List<Trainer> getAllTrainers() {
         return trainerService.getAllTrainers();
     }
     
     @GetMapping("/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public Trainer getTrainerById(@PathVariable Long id) {
         return trainerService.getTrainerById(id);
     }
